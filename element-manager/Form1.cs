01788eb605a1e7dd26871b2a0edf83298d5d240e
socra167@gmail.com <socra167@gmail.com>
2022-12-30 12:31:00 +0900

    초기 커밋.

---
+++ element-manager/Form1.cs
@@ -0,0 +1,10 @@
+namespace element_manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ element-manager/Program.cs
@@ -0,0 +1,12 @@
+namespace element_manager
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
