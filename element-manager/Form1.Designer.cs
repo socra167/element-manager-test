0359ba13bb8c633d3ba51089803411a0e881d67a
socra167@gmail.com <socra167@gmail.com>
2023-01-02 14:58:02 +0900

    feat: Login 화면 추가

--- element-manager/Form1.Designer.cs
+++ element-manager/Form1.Designer.cs
@@ -28,19 +28,224 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.minButton = new FontAwesome.Sharp.IconButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.closeButton = new FontAwesome.Sharp.IconButton();
+            this.maxButton = new FontAwesome.Sharp.IconButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.signUpLink = new System.Windows.Forms.LinkLabel();
+            this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
+            this.textPW = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(60)))), ((int)(((byte)(70)))));
+            this.panel1.Controls.Add(this.minButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.closeButton);
+            this.panel1.Controls.Add(this.maxButton);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(330, 50);
+            this.panel1.TabIndex = 1;
+            // 
+            // minButton
+            // 
+            this.minButton.FlatAppearance.BorderSize = 0;
+            this.minButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minButton.IconChar = FontAwesome.Sharp.IconChar.WindowMinimize;
+            this.minButton.IconColor = System.Drawing.Color.White;
+            this.minButton.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            this.minButton.IconSize = 20;
+            this.minButton.Location = new System.Drawing.Point(246, 12);
+            this.minButton.Name = "minButton";
+            this.minButton.Size = new System.Drawing.Size(25, 25);
+            this.minButton.TabIndex = 4;
+            this.minButton.UseVisualStyleBackColor = true;
+            this.minButton.Click += new System.EventHandler(this.minButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("나눔스퀘어라운드OTF Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // closeButton
+            // 
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.IconChar = FontAwesome.Sharp.IconChar.X;
+            this.closeButton.IconColor = System.Drawing.Color.White;
+            this.closeButton.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            this.closeButton.IconSize = 20;
+            this.closeButton.Location = new System.Drawing.Point(302, 12);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(25, 25);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // maxButton
+            // 
+            this.maxButton.FlatAppearance.BorderSize = 0;
+            this.maxButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.maxButton.IconChar = FontAwesome.Sharp.IconChar.Square;
+            this.maxButton.IconColor = System.Drawing.Color.White;
+            this.maxButton.IconFont = FontAwesome.Sharp.IconFont.Regular;
+            this.maxButton.IconSize = 20;
+            this.maxButton.Location = new System.Drawing.Point(274, 12);
+            this.maxButton.Name = "maxButton";
+            this.maxButton.Size = new System.Drawing.Size(25, 25);
+            this.maxButton.TabIndex = 3;
+            this.maxButton.UseVisualStyleBackColor = true;
+            this.maxButton.Click += new System.EventHandler(this.maxButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("나눔스퀘어라운드OTF ExtraBold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(0, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(330, 46);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Element Manager";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textID
+            // 
+            this.textID.Font = new System.Drawing.Font("맑은 고딕", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textID.Location = new System.Drawing.Point(69, 128);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(221, 43);
+            this.textID.TabIndex = 6;
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.User;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            this.iconPictureBox1.IconSize = 31;
+            this.iconPictureBox1.Location = new System.Drawing.Point(32, 135);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(32, 31);
+            this.iconPictureBox1.TabIndex = 8;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.loginBtn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.loginBtn.Font = new System.Drawing.Font("나눔고딕OTF", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.loginBtn.Location = new System.Drawing.Point(59, 267);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(221, 29);
+            this.loginBtn.TabIndex = 11;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = false;
+            // 
+            // signUpLink
+            // 
+            this.signUpLink.AutoSize = true;
+            this.signUpLink.Font = new System.Drawing.Font("나눔고딕OTF", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.signUpLink.Location = new System.Drawing.Point(218, 237);
+            this.signUpLink.Name = "signUpLink";
+            this.signUpLink.Size = new System.Drawing.Size(53, 15);
+            this.signUpLink.TabIndex = 12;
+            this.signUpLink.TabStop = true;
+            this.signUpLink.Text = "Sign up";
+            // 
+            // iconPictureBox2
+            // 
+            this.iconPictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.iconPictureBox2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.Key;
+            this.iconPictureBox2.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox2.IconFont = FontAwesome.Sharp.IconFont.Solid;
+            this.iconPictureBox2.Location = new System.Drawing.Point(31, 183);
+            this.iconPictureBox2.Name = "iconPictureBox2";
+            this.iconPictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.iconPictureBox2.TabIndex = 9;
+            this.iconPictureBox2.TabStop = false;
+            // 
+            // textPW
+            // 
+            this.textPW.Font = new System.Drawing.Font("맑은 고딕", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textPW.Location = new System.Drawing.Point(69, 177);
+            this.textPW.Name = "textPW";
+            this.textPW.Size = new System.Drawing.Size(221, 43);
+            this.textPW.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(43, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "New to Element Manager?";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(330, 338);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textPW);
+            this.Controls.Add(this.signUpLink);
+            this.Controls.Add(this.iconPictureBox2);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.iconPictureBox1);
+            this.Controls.Add(this.textID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private FontAwesome.Sharp.IconButton closeButton;
+        private FontAwesome.Sharp.IconButton minButton;
+        private FontAwesome.Sharp.IconButton maxButton;
+        private Label label4;
+        private TextBox textID;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private Button loginBtn;
+        private LinkLabel signUpLink;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
+        private TextBox textPW;
+        private Label label2;
     }
 }
--- element-manager/Form1.cs
+++ element-manager/Form1.cs
@@ -6,5 +6,30 @@ namespace element_manager
         {
             InitializeComponent();
         }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maxButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
